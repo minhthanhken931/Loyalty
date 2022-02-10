e1bad028502d4830af053dfdc205b32d8d304502
minhthanhken931 <minhthanhken931@gmail.com>
2022-02-10 22:40:07 +0700

    update new project

---
+++ demo/demo/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ demo/demo/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection filed)
+        {
+            string error = "";
+            string user = filed["email"];
+            string password = filed["password"];
+            if (user == "master@gmail.com" && password == "246810")
+            {
+                Session["UserAdmin"] = "master@gmail.com";
+                return RedirectToAction("Index", "us");
+            }
+            else
+            {
+                if (user !="master@gmail.com" && password !="246810")
+                {
+                    error = "Email or Password is incorrect ! ";
+                }
+                if (user == "" && password == "")
+                {
+                    error = "Email or Password is not empty !";
+                }    
+                
+            }
+            ViewBag.StrError = "<div class = 'text-danger'>* " + error + "</div>";
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["UserAdmin"] = "";
+            return RedirectToAction("Index", "Home");
+        }
+
+        // Admin/Auth/Forgot_password
+        public ActionResult Forgot_password()
+        {
+            ViewBag.StrError = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Forgot_password(FormCollection filed)
+        {
+            return View();
+        }
+
+
+
+    }
+}
